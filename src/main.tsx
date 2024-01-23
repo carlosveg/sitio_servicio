254aa2b5d0819fcbbfa97639f5a60142426b53e9
carlos vega <carlosvegapro@gmail.com>
2024-01-23 15:27:22 -0600

    [fix] Se eliminan imports sin usar

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,14 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
-import {
-  BrowserRouter,
-  HashRouter,
-  RouterProvider,
-  createHashRouter
-} from 'react-router-dom'
-import Blog from './pages/home/Blog.tsx'
-import CPP from './pages/CPP/CPP.tsx'
 
-const router = createHashRouter([
-  { path: '/', element: <Blog /> },
-  { path: '/c++', element: <CPP title='CPP' /> }
-])
+// const router = createHashRouter([
+//   { path: '/', element: <Blog /> },
+//   { path: '/c++', element: <CPP title='CPP' /> }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
